59a18d4372e1fd402d11c1c5713550d8a0a09619
TARBOOXBLACK <163377116+TARBOOXBLACK@users.noreply.github.com>
2024-03-14 06:24:34 +0200

    Add files via upload

---
+++ plugins/افلام ثقافيه.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, command, text }) => {
+let love = `
+*✥━─━⌬〘👾〙⌬━─━✥*
+⬪  ࣪    ࣭     ࣪     ࣭   𝅦𝅦   ࣭     ࣪     ࣭    ࣪  ⬫
+⬪  ࣪    ࣭     ࣪     ࣭   𝅦𝅦   ࣭     ࣪     ࣭    ࣪  ⬫
+⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪▭̷໋̟  ۫ 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 ۫  ▭̷໋̟۪۬໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪   
+⬪  ࣪    ࣭     ࣪     ࣭   𝅦𝅦   ࣭     ࣪     ࣭    ࣪  ⬫
+⬪  ࣪    ࣭     ࣪     ࣭   𝅦𝅦   ࣭     ࣪     ࣭    ࣪  ⬫‏
+*✥━─━⌬〘👾〙⌬━─━✥*
+⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑࣪ 𝐾𝐿𝐸𝑉𝐸𝑅﴾𝐵𝑂𝑇﴿🍥̷໋̟۪۬໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪
+*✥━─━⌬〘👾〙⌬━─━✥*
+
+*⌬〘 مرحبا بك في بوت كليفر 〙⌬*
+
+*⌬〘 اليك قائمه الافلام الاباحيه 〙⌬*
+
+*✥━─━⌬〘👾〙⌬━─━✥*
+
+*❬👾❭↜ 〚 .xvideos 〛*
+*❬👾❭↜ 〚 .videoxxx 〛*
+*❬👾❭↜ 〚 .hornymenu 〛*
+*❬👾❭↜ 〚 .pack 〛*
+*❬👾❭↜ 〚 .pack2 〛*
+*❬👾❭↜ 〚 .pack3 〛*
+*❬👾❭↜ 〚 .xnxxdl 〛*
+*❬👾❭↜ 〚 .pornoalevid 〛*
+*❬👾❭↜ 〚 .pornovidbi 〛*
+*❬👾❭↜ 〚 .Pornolesv 〛*
+*❬👾❭↜ 〚 .pornovidg 〛*
+*❬👾❭↜ 〚 .Pornovideo 〛*
+*❬👾❭↜ 〚 .xnxxbuscar 〛*
+*❬👾❭↜ 〚 .xnxxsearch 〛*
+*❬👾❭↜ 〚 .nsfw 〛*
+
+*✥━─━⌬〘👾〙⌬━─━✥*
+ `.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(سكس|xxx|xnxx)$/i
+export default handler
